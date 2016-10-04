6b0ee13120585b35102357efd60fd0d240cb800f
andyaoe <andy@getdoorbot.com>
2016-10-03 19:54:32 -0700

    reading adc readings in loop

--- main.c
+++ main.c
@@ -58,8 +58,97 @@ encountered \n",__FILE__, __LINE__, __FUNCTION__);exit(1);}else{;}};
 #define CHANNEL_TO_OPEN			0	/*0 for first available channel, 1 for next... */
 #define DATA_OFFSET				1
 
-#define TSL2591_COMMAND_BIT         (0xA0)    // 1010 0000: bits 7 and 5 for 'command normal'
-#define TSL2591_REGISTER_DEVICE_ID  (0x12)
+/******************************************************************************/
+/*								TSL2591 Specific Macro and type defines							   */
+/******************************************************************************/
+#define TSL2591_VISIBLE           (2)       // channel 0 - channel 1
+#define TSL2591_INFRARED          (1)       // channel 1
+#define TSL2591_FULLSPECTRUM      (0)       // channel 0
+
+#define TSL2591_ADDR              (0x29)
+#define TSL2591_READBIT           (0x01)
+
+#define TSL2591_COMMAND_BIT       (0xA0)    // 1010 0000: bits 7 and 5 for 'command normal'
+#define TSL2591_CLEAR_INT         (0xE7)
+#define TSL2591_TEST_INT          (0xE4)
+#define TSL2591_WORD_BIT          (0x20)    // 1 = read/write word (rather than byte)
+#define TSL2591_BLOCK_BIT         (0x10)    // 1 = using block read/write
+
+#define TSL2591_ENABLE_POWEROFF   (0x00)
+#define TSL2591_ENABLE_POWERON    (0x01)
+#define TSL2591_ENABLE_AEN        (0x02)    // ALS Enable. This field activates ALS function. Writing a one activates the ALS. Writing a zero disables the ALS.
+#define TSL2591_ENABLE_AIEN       (0x10)    // ALS Interrupt Enable. When asserted permits ALS interrupts to be generated, subject to the persist filter.
+#define TSL2591_ENABLE_NPIEN      (0x80)    // No Persist Interrupt Enable. When asserted NP Threshold conditions will generate an interrupt, bypassing the persist filter
+
+#define TSL2591_LUX_DF            (408.0F)
+#define TSL2591_LUX_COEFB         (1.64F)  // CH0 coefficient
+#define TSL2591_LUX_COEFC         (0.59F)  // CH1 coefficient A
+#define TSL2591_LUX_COEFD         (0.86F)  // CH2 coefficient B
+
+enum
+{
+  TSL2591_REGISTER_ENABLE             = 0x00,
+  TSL2591_REGISTER_CONTROL            = 0x01,
+  TSL2591_REGISTER_THRESHOLD_AILTL    = 0x04, // ALS low threshold lower byte
+  TSL2591_REGISTER_THRESHOLD_AILTH    = 0x05, // ALS low threshold upper byte
+  TSL2591_REGISTER_THRESHOLD_AIHTL    = 0x06, // ALS high threshold lower byte
+  TSL2591_REGISTER_THRESHOLD_AIHTH    = 0x07, // ALS high threshold upper byte
+  TSL2591_REGISTER_THRESHOLD_NPAILTL  = 0x08, // No Persist ALS low threshold lower byte
+  TSL2591_REGISTER_THRESHOLD_NPAILTH  = 0x09, // etc
+  TSL2591_REGISTER_THRESHOLD_NPAIHTL  = 0x0A,
+  TSL2591_REGISTER_THRESHOLD_NPAIHTH  = 0x0B,
+  TSL2591_REGISTER_PERSIST_FILTER     = 0x0C,
+  TSL2591_REGISTER_PACKAGE_PID        = 0x11,
+  TSL2591_REGISTER_DEVICE_ID          = 0x12,
+  TSL2591_REGISTER_DEVICE_STATUS      = 0x13,
+  TSL2591_REGISTER_CHAN0_LOW          = 0x14,
+  TSL2591_REGISTER_CHAN0_HIGH         = 0x15,
+  TSL2591_REGISTER_CHAN1_LOW          = 0x16,
+  TSL2591_REGISTER_CHAN1_HIGH         = 0x17
+};
+
+typedef enum
+{
+  TSL2591_INTEGRATIONTIME_100MS     = 0x00,
+  TSL2591_INTEGRATIONTIME_200MS     = 0x01,
+  TSL2591_INTEGRATIONTIME_300MS     = 0x02,
+  TSL2591_INTEGRATIONTIME_400MS     = 0x03,
+  TSL2591_INTEGRATIONTIME_500MS     = 0x04,
+  TSL2591_INTEGRATIONTIME_600MS     = 0x05,
+}
+tsl2591IntegrationTime_t;
+
+typedef enum
+{
+  //  bit 7:4: 0
+  TSL2591_PERSIST_EVERY             = 0x00, // Every ALS cycle generates an interrupt
+  TSL2591_PERSIST_ANY               = 0x01, // Any value outside of threshold range
+  TSL2591_PERSIST_2                 = 0x02, // 2 consecutive values out of range
+  TSL2591_PERSIST_3                 = 0x03, // 3 consecutive values out of range
+  TSL2591_PERSIST_5                 = 0x04, // 5 consecutive values out of range
+  TSL2591_PERSIST_10                = 0x05, // 10 consecutive values out of range
+  TSL2591_PERSIST_15                = 0x06, // 15 consecutive values out of range
+  TSL2591_PERSIST_20                = 0x07, // 20 consecutive values out of range
+  TSL2591_PERSIST_25                = 0x08, // 25 consecutive values out of range
+  TSL2591_PERSIST_30                = 0x09, // 30 consecutive values out of range
+  TSL2591_PERSIST_35                = 0x0A, // 35 consecutive values out of range
+  TSL2591_PERSIST_40                = 0x0B, // 40 consecutive values out of range
+  TSL2591_PERSIST_45                = 0x0C, // 45 consecutive values out of range
+  TSL2591_PERSIST_50                = 0x0D, // 50 consecutive values out of range
+  TSL2591_PERSIST_55                = 0x0E, // 55 consecutive values out of range
+  TSL2591_PERSIST_60                = 0x0F, // 60 consecutive values out of range
+}
+tsl2591Persist_t;
+
+typedef enum
+{
+  TSL2591_GAIN_LOW                  = 0x00,    // low gain (1x)
+  TSL2591_GAIN_MED                  = 0x10,    // medium gain (25x)
+  TSL2591_GAIN_HIGH                 = 0x20,    // medium gain (428x)
+  TSL2591_GAIN_MAX                  = 0x30,    // max gain (9876x)
+}
+tsl2591Gain_t;
+
 
 /******************************************************************************/
 /*								Global variables							  	    */
@@ -69,6 +158,8 @@ FT_HANDLE ftHandle;
 ChannelConfig channelConf;
 FT_STATUS status;
 uint8 buffer[I2C_DEVICE_BUFFER_SIZE];
+uint8 _integration = TSL2591_INTEGRATIONTIME_600MS;
+uint8 _gain        = TSL2591_GAIN_MED;
 
 /******************************************************************************/
 /*						Public function definitions						  		   */
@@ -88,7 +179,7 @@ uint8 buffer[I2C_DEVICE_BUFFER_SIZE];
  * \warning
  */
 
-FT_STATUS write_byte(uint8 slaveAddress, uint8 registerAddress, uint8 data)
+FT_STATUS write8(uint8 registerAddress, uint8 data)
 {
 	uint32 bytesToTransfer = 0;
 	uint32 bytesTransfered;
@@ -99,7 +190,7 @@ FT_STATUS write_byte(uint8 slaveAddress, uint8 registerAddress, uint8 data)
 	bytesTransfered=0;
 	buffer[bytesToTransfer++]=registerAddress; /* Byte addressed inside EEPROM */
 	buffer[bytesToTransfer++]=data;
-	status = I2C_DeviceWrite(ftHandle, slaveAddress, bytesToTransfer, buffer, \
+	status = I2C_DeviceWrite(ftHandle, 0x29, bytesToTransfer, buffer, \
 &bytesTransfered, I2C_TRANSFER_OPTIONS_START_BIT|I2C_TRANSFER_OPTIONS_STOP_BIT);
 
 	/* poll to check completition */
@@ -108,7 +199,7 @@ FT_STATUS write_byte(uint8 slaveAddress, uint8 registerAddress, uint8 data)
 		bytesToTransfer=0;
 		bytesTransfered=0;
 		buffer[bytesToTransfer++]=registerAddress; /* Addressed inside EEPROM  */
-		status = I2C_DeviceWrite(ftHandle, slaveAddress, bytesToTransfer,\
+		status = I2C_DeviceWrite(ftHandle, 0x29, bytesToTransfer,\
 			buffer, &bytesTransfered, \
 			I2C_TRANSFER_OPTIONS_START_BIT|I2C_TRANSFER_OPTIONS_BREAK_ON_NACK);
 		if((FT_OK == status) && (bytesToTransfer == bytesTransfered))
@@ -135,7 +226,7 @@ FT_STATUS write_byte(uint8 slaveAddress, uint8 registerAddress, uint8 data)
  * \note
  * \warning
  */
-FT_STATUS read_byte(uint8 slaveAddress, uint8 registerAddress, uint8 *data)
+FT_STATUS read8(uint8 registerAddress, uint8 *data)
 {
 	FT_STATUS status;
 	uint32 bytesToTransfer = 0;
@@ -144,16 +235,79 @@ FT_STATUS read_byte(uint8 slaveAddress, uint8 registerAddress, uint8 *data)
 	bytesToTransfer=0;
 	bytesTransfered=0;
 	buffer[bytesToTransfer++]=registerAddress; /*Byte addressed inside EEPROM */
-	status = I2C_DeviceWrite(ftHandle, slaveAddress, bytesToTransfer, buffer, \
+	status = I2C_DeviceWrite(ftHandle, 0x29, bytesToTransfer, buffer, \
 		&bytesTransfered, I2C_TRANSFER_OPTIONS_START_BIT);
 	APP_CHECK_STATUS(status);
 	bytesToTransfer=1;
 	bytesTransfered=0;
-	status |= I2C_DeviceRead(ftHandle, slaveAddress, bytesToTransfer, buffer, \
+	status |= I2C_DeviceRead(ftHandle, 0x29, bytesToTransfer, buffer, \
 		&bytesTransfered, I2C_TRANSFER_OPTIONS_START_BIT);
 	APP_CHECK_STATUS(status);
 	*data = buffer[0];
 	return status;
+}
+
+uint16 read16(uint8 registerAddress)
+{
+	FT_STATUS status;
+	uint16 data = 0;
+	uint32 bytesToTransfer = 0;
+	uint32 bytesTransfered;
+
+	bytesToTransfer=0;
+	bytesTransfered=0;
+	buffer[bytesToTransfer++]=registerAddress; /*Byte addressed inside EEPROM */
+	status = I2C_DeviceWrite(ftHandle, 0x29, bytesToTransfer, buffer, \
+		&bytesTransfered, I2C_TRANSFER_OPTIONS_START_BIT);
+	APP_CHECK_STATUS(status);
+	bytesToTransfer=2;
+	bytesTransfered=0;
+	status |= I2C_DeviceRead(ftHandle, 0x29, bytesToTransfer, buffer, \
+		&bytesTransfered, I2C_TRANSFER_OPTIONS_START_BIT);
+	APP_CHECK_STATUS(status);
+	data = buffer[0] | ((uint16)buffer[1] << 8);
+	return data;
+}
+
+
+uint32 getFullLuminosity (void)
+{
+  // Wait x ms for ADC to complete
+  for (uint8 d=0; d<=_integration; d++)
+  {
+        Sleep(120);
+  }
+
+  uint32 x;
+  x = read16(TSL2591_COMMAND_BIT | TSL2591_REGISTER_CHAN1_LOW);
+  x <<= 16;
+  x |= read16(TSL2591_COMMAND_BIT | TSL2591_REGISTER_CHAN0_LOW);
+
+  return x;
+}
+
+uint16 getLuminosity (uint8 channel)
+{
+  uint32 x = getFullLuminosity();
+
+  if (channel == TSL2591_FULLSPECTRUM)
+  {
+    // Reads two byte value from channel 0 (visible + infrared)
+    return (x & 0xFFFF);
+  }
+  else if (channel == TSL2591_INFRARED)
+  {
+    // Reads two byte value from channel 1 (infrared)
+    return (x >> 16);
+  }
+  else if (channel == TSL2591_VISIBLE)
+  {
+    // Reads all and subtracts out just the visible!
+    return ( (x & 0xFFFF) - (x >> 16));
+  }
+
+  // unknown channel!
+  return 0;
 }
 
 /*!
@@ -212,9 +366,26 @@ int main()
 		status = I2C_InitChannel(ftHandle,&channelConf);
 		APP_CHECK_STATUS(status);
 
-        address = TSL2591_COMMAND_BIT | TSL2591_REGISTER_DEVICE_ID;
-        read_byte(0x29, address, &data);
-        printf("data: 0x%x\n", data);
+        //read the id
+        read8(TSL2591_COMMAND_BIT | TSL2591_REGISTER_DEVICE_ID, &data);
+        printf("id: 0x%x\n", data);
+
+        //set the integration and gain
+        write8(TSL2591_COMMAND_BIT | TSL2591_REGISTER_CONTROL, _integration | _gain);
+
+        // Enable the device by setting the control bit to 0x01
+        write8(TSL2591_COMMAND_BIT | TSL2591_REGISTER_ENABLE, TSL2591_ENABLE_POWERON | TSL2591_ENABLE_AEN);
+
+        for (uint8 i = 0; i < 200; i++)
+        {
+            // get counts
+            printf("IR counts: %u\n", getLuminosity(TSL2591_INFRARED));
+        }
+
+
+
+
+
 
         /*
 		for(address=START_ADDRESS_EEPROM;address<END_ADDRESS_EEPROM;address++)
